ed6f3b25772153818de1c52078b67bdb58164114
Vadim Yanko <yanko.vadim@gmail.com>
2019-11-16 14:41:11 +0200

    getting title

--- core/youtube.py
+++ core/youtube.py
@@ -1,14 +1,38 @@
+import os
+
+from youtube_api import YouTubeDataAPI
+
+YOUTUBE_API_KEY = os.getenv('YOUTUBE_API_KEY')
+
 
 def is_youtube_link(text):
     return any(keyword in text for keyword in ('youtu.be', 'youtube'))
 
 
-class YouTubeConverter:
-    @classmethod
-    def get_title(cls, link):
-        return 'Getting title for YouTube link...'
+def _strip_video_id_from_url(url):
+    '''Strips the video_id from YouTube URL.'''
+    if '/watch?v=' in url.lower():
+        url_ = (url.split('&v=')[-1].split('/watch?v=')[-1].split('?')[0].split('&')[0])
 
-    @classmethod
-    def get_link(cls, search_text):
+    elif 'youtu.be' in url.lower():
+        url_ = url[url.rindex('/') + 1:]
+        if '?' in url_:
+            url_ = url_[:url_.rindex('?')]
+    else:
+        url_ = None
+
+    return url_
+
+
+class YouTubeConverter:
+    def __init__(self):
+        self._client = YouTubeDataAPI(YOUTUBE_API_KEY)
+
+    def get_title(self, link):
+        id_ = _strip_video_id_from_url(link)
+        metadata = self._client.get_video_metadata(id_)
+        return metadata['video_title']
+
+    def get_link(self, search_text):
         # TODO: search in youtube for `search_text`
         return 'Converting to YouTube link...'
