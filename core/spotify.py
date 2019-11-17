f1797f71b8744b06bc6bed003f831b940e72236a
Vadim Yanko <yanko.vadim@gmail.com>
2019-11-17 20:00:52 +0200

    Implemented search in spotify

--- core/spotify.py
+++ core/spotify.py
@@ -1,4 +1,10 @@
+import os
+
 import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
+CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
 
 
 def is_spotify_link(text):
@@ -9,10 +15,13 @@ class SpotifyConverter:
     _client = None
 
     def __init__(self):
-        self._client = spotipy.Spotify()
+        credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+        self._client = spotipy.Spotify(client_credentials_manager=credentials_manager)
 
     def get_search_query(self, link):
         return 'Getting title for Spotify link...'
 
     def get_link(self, search_text):
-        return 'Looking for Spotify link'
+        res = self._client.search(q=search_text, limit=1, type='track')
+        track = res['tracks']['items'][0]
+        return track['external_urls']['spotify']
