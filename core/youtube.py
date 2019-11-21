92020cbc68e548eaab17da79b65d099d03aba914
Vadim Yanko <yanko.vadim@gmail.com>
2019-11-21 03:47:57 +0200

    Removing feats and audio

--- core/youtube.py
+++ core/youtube.py
@@ -40,7 +40,7 @@ def _get_url_from_video_id(video_id):
 def clean_video_title(video_title):
     # Ed Sheeran - Beautiful People (feat. Khalid) [Official Video]
     # Lizzo - Good As Hell (Official Video)
-    pollution_words_group = r'(?:official|video)'
+    pollution_words_group = r'(?:official|video|audio|ft|feat)'
     search_pattern = rf'(?:\(.*{pollution_words_group}.*\)|\[.*{pollution_words_group}.*\]|\|.*)'
     compiled_pattern = re.compile(search_pattern, re.IGNORECASE)
     return re.sub(compiled_pattern, lambda x: '', video_title).strip()
--- tests/test_youtube.py
+++ tests/test_youtube.py
@@ -5,10 +5,12 @@ from core.youtube import clean_video_title
 
 @pytest.mark.parametrize("link,cleaned_link", [
     ('(Official Video) hello [Official New Audio Video]', 'hello'),
-    ('Ed Sheeran - Beautiful People (feat. Khalid) [Official Video]', 'Ed Sheeran - Beautiful People (feat. Khalid)'),
+    ('Ed Sheeran - Beautiful People (feat. Khalid) [Official Video]', 'Ed Sheeran - Beautiful People'),
     ('Lizzo - Good As Hell (Official Video)', 'Lizzo - Good As Hell'),
     ('True Damage - GIANTS (ft. Becky G, Keke Palmer, SOYEON, DUCKWRTH, Thutmose) | League of Legends',
-     'True Damage - GIANTS (ft. Becky G, Keke Palmer, SOYEON, DUCKWRTH, Thutmose)'),
+     'True Damage - GIANTS'),
+    ('Gryffin, Slander - All You Need To Know (Audio) ft. Calle Lehmann',
+     'Gryffin, Slander - All You Need To Know  ft. Calle Lehmann')
 ])
 def test_video_title_cleaning(link, cleaned_link):
     assert clean_video_title(link) == cleaned_link
